859746e95b36866dd64cad4bac162595feea2ac1
Kyle W <kylew864@gmail.com>
2021-10-19 09:32:37 -0400

    progress

--- server/game.js
+++ server/game.js
@@ -18,15 +18,36 @@ function onPeerConnect(peerContext, context) {
 function onPeerDisconnect(peerContext, context) {}
 
 function onPeerData(data, peerContext, context) {
-  let send = (d) => peerContext.peer.send(d)
-  if (data == "ping") {
+  let send = (d) => {
+    try {
+      peerContext.peer.send(d)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  // Ping
+  if (data == "!ping") {
     send("pong");
     return;
   }
-  if (data == "pong") {
-    send("ping");
+
+  // Get player location data + username + id
+  if (data == "!players") {
+    let players = context.map(peerData => {
+      let p = peerData.ipInfo;
+      return {
+        id: peerData.scratchpad.id,
+        username: p.ka_username,
+        loc: p.loc,
+        country: p.country,
+        timezone: p.timezone,
+      }
+    })
+    send(JSON.stringify(players))
     return;
   }
+
   let num = parseInt(data)
   if (isNaN(num)) {
     console.log("Not a number", data);
