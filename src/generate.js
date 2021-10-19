d519051f62bf4c9d59fc0914d58e0c905659d00e
Kyle W <kylew864@gmail.com>
2021-10-19 11:54:44 -0400

    a

---
+++ src/generate.js
@@ -0,0 +1,17 @@
+let getMenuCode = (status, players, games, version, isInRoom) => `
+ _______  __   __  _______  __    _  _______    __   __  _______ 
+|   _   ||  |_|  ||       ||  |  | ||       |  |  | |  ||       |
+|  |_|  ||       ||   _   ||   |_| ||    ___|  |  | |  ||  _____|
+|       ||       ||  | |  ||       ||   | __   |  |_|  || |_____ 
+|       ||       ||  |_|  ||  _    ||   ||  |  |       ||_____  |
+|   _   || ||_|| ||       || | |   ||   |_| |  |       | _____| |
+|__| |__||_|   |_||_______||_|  |__||_______|  |_______||_______|
+                                                     
+ KA Edition 
+
+${isInRoom ? "-" : "*"} [Host]                  Status    ${status}${" ".repeat(10-status.length)}Special thanks to
+${isInRoom ? "-" : "*"} [Join random]           Players   ${players}${" ".repeat(2-players.length)}           everyone who made
+${isInRoom ? "*" : "-"} [Leave]                 Games     ${games}            this possible!
+* [Credits]
+* [Exit game]             Version   ${version}`
+
